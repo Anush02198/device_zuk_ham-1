74d8ce6e4a6b15e9d107a15f6852559f29fbe007
leodiram <sriramscience7@gmail.com>
2018-06-06 20:37:15 +0530

    ham : build vendor camera
    this r required by IMS
    
    Signed-off-by: leodiram <sriramscience7@gmail.com>

--- treble.mk
+++ treble.mk
@@ -27,9 +27,13 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
+    android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service.ham \
-    camera.device@1.0-impl-legacy
+    camera.device@1.0-impl \
+    camera.device@3.2-impl \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor
+
 
 # DRM
 PRODUCT_PACKAGES += \
